8a2f79c3d829eea3753c883dfa5856747b3c4bab
Peng Du <rick.peng.du@gmail.com>
2020-01-05 23:57:42 -0800

    add beer print

--- class1/99BottlesOfBeers/src/Main.java
+++ class1/99BottlesOfBeers/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // print out the 99 bottles lyric from http://www.99-bottles-of-beer.net/lyrics.html
+        for (int i=99; i>0; i--) {
+            System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            if (i>1) {
+                System.out.println("Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.\n");
+            } else {
+                System.out.println("Take one down and pass it around, no more bottles of beer on the wall.\n");
+            }
+        }
+
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.");
     }
 }
