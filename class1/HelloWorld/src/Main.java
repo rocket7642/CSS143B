6a4f2b0fbfd5ffdbfe5b5dec7dfc76a5c2667333
Peng Du <rick.peng.du@gmail.com>
2020-01-06 00:25:07 -0800

    initial updates for bubble sort

--- class1/HelloWorld/src/Main.java
+++ class1/HelloWorld/src/Main.java
@@ -2,5 +2,35 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // variables
+        int myIntegerVariable;
+        myIntegerVariable = 10;
+
+        // self increment
+        myIntegerVariable++;
+        System.out.printf("%d", myIntegerVariable);
+
+        // array
+        int myArray[];
+        myArray = new int[myIntegerVariable];
+        for (int i=0; i<myIntegerVariable; i++) {
+            myArray[i] = i*2;
+        }
+
+        // if statement
+        if (myIntegerVariable>10) {
+            System.out.println("myIntegerVariable larger than 10");
+        } else {
+            System.out.println("myIntegerVariable smaller or equal to 10");
+        }
+
+        // loop
+        for (int i=0; i<myIntegerVariable; i++) {
+            System.out.printf("myArray[%d] = %d\n", i, myArray[i]);
+        }
     }
 }
+
+
+
