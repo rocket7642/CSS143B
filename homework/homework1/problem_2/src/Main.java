7453fffe10fa0bd98ed2c69d1be8e4bd53ff0756
Peng Du <rick.peng.du@gmail.com>
2020-01-09 19:45:15 -0800

    rename bubble sort function

--- homework/homework1/problem_2/src/Main.java
+++ homework/homework1/problem_2/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("testBubbleSort " + (testBubbleSort() ? "PASSED" : "FAILED"));
     }
 
-    public static void findHighestFrequencyNumber(int[] data) {
+    public static void bubbleSort(int[] data) {
         // homework
         return; // place holder
     }
