44aa630f117df0aaf9861d1ab066bb771dc45513
Peng Du <rick.peng.du@gmail.com>
2020-01-31 01:49:26 -0800

    fix code

--- class5/DreamInDream/src/Main.java
+++ class5/DreamInDream/src/Main.java
@@ -3,11 +3,12 @@ public class Main {
     private static void dream(int level) {
         System.out.println("started dreaming in level " + level);
 
+        if (level > 0) {
             System.out.println("pause everything. going into " +
-								(level-1) + " level of dream");
-            dream(level-1);
-            System.out.println("woke up from " + (level-1) +
-                    " level of dream, continue in level " + level);
+                    (level - 1) + " level of dream");
+            dream(level - 1);
+            System.out.println("woke up from " + (level - 1) + " level of dream, continue in level " + level);
+        }
 
         System.out.println("waking up from dream level " + level);
     }
