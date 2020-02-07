e818b2cae6e30d570336b6d60fa9fac61587a8f0
Peng Du <rick.peng.du@gmail.com>
2020-02-06 17:19:10 -0800

    update

--- homework/homework4/problem_2/QuickSort/src/Main.java
+++ homework/homework4/problem_2/QuickSort/src/Main.java
@@ -17,15 +17,7 @@ public class Main {
     }
 
     public static void bubbleSort(int data[]) {
-        for (int i = 0; i < data.length - 1; i++) {
-            for (int j = 0; j < data.length - i - 1; j++) {
-                if (data[j] > data[j + 1]) {
-                    int temp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = temp;
-                }
-            }
-        }
+        // use code from homework 1
     }
 
     public static void quickSort(int data[]) {
