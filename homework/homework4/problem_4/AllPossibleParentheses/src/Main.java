7775f699f14dae9cd33651c3e6525e5c19049026
Peng Du <rick.peng.du@gmail.com>
2020-02-06 17:45:32 -0800

    add problem 4

---
+++ homework/homework4/problem_4/AllPossibleParentheses/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    // helper function
+    private void possibleParentheses(int pos, int n, int open, int close) {
+        // homework
+    }
+
+    private void possibleParentheses(int n) {
+        // homework
+    }
+
+    public static void main(String[] args) {
+       testPossibleParentheses();
+    }
+
+    private static void testPossibleParentheses() {
+
+    }
+}
---
+++ homework/homework4/problem_4/readme.md
@@ -0,0 +1,9 @@
+For example
+- when n = 1, *result* will consist of only 1 string: "{}"
+- when n = 2, *result* will consist of 2 strings: '{}{}' and "{{}}"
+- when n = 3, *result* will contain the following 5 strings:
+"{}{}{}", 
+"{}{{}}",
+"{{}}{}",
+"{{}{}}",
+"{{{}}}"
